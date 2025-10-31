ab2c36e4d6531ae0584bb46b0d906cf6c976cae6
finnensley <finniganensley@gmail.com>
2025-10-31 16:15:09 -0600

    visual updates to dashboard

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -21,48 +21,68 @@ const DashboardPage = () => {
 
   return (
     <div>
-      <div>
-        <div className="flex flex-col ml-2 mr-2 mt-4 p-4 border rounded-lg border-y  text-white text-lg text-shadow-lg font-semibold items-center justify-center">
-          <div>Daily Output, with week/month/year toggle: </div>
-        </div>
-        {/* Ready to Ship & Picked side by side */}
-        <div className="flex flex-row justify-evenly ml-2 mr-2 mt-4 p-4 border rounded-lg border-y  text-white text-lg text-shadow-lg font-semibold items-center">
-          <div className="flex flex-row gap-4">
-            <div className="flex flex-col bg-gray-800/30 shadow-md shadow-white p-4 rounded-lg">
-              <h1 className="text-2xl">READY TO SHIP:</h1>
-              <p className="ml-2 text-xl"> Orders: {openOrderCount} </p>
-              <p className="ml-2 text-xl">Total Revenue: ${orderTotal} </p>
-              <p>Number of Orders by Type/Revenue: </p>
-            </div>
-            <div className="flex flex-col bg-gray-800/30 shadow-md shadow-white p-4 rounded-lg">
-              <h1 className="text-2xl">PICKED:</h1>
-              <p className="ml-2 text-xl"> Orders: {/*{pickedOrderCount}*/} </p>
-              <p className="ml-2 text-xl">
-                Total Revenue: {/*${pickedOrderTotal}*/}
-              </p>
-              <p>Number of Orders by Type/Revenue: </p>
-            </div>
-          </div>
-        </div>
-        {/* Shipped below, full width */}
-        <div className="flex flex-row justify-evenly ml-2 mr-2 mt-4 p-4 border rounded-lg border-y  text-white text-lg text-shadow-lg font-semibold items-center">
-          <div className="flex flex-col bg-gray-800/40 shadow-md shadow-white p-4 rounded-lg">
-            <h1 className="text-2xl">SHIPPED:</h1>
-            <p className="ml-2 text-xl"> Orders: {/*{shippedOrderCount}*/} </p>
-            <p className="ml-2 text-xl">
-              Total Revenue: {/*${shippedOrderTotal}*/}
-            </p>
-            <p>Shipping Revenue vs Shipping Cost</p>
-          </div>
-        </div>
-        {/* Inventory Health & Needs Replenishment side by side */}
-        <div className="flex flex-row justify-evenly ml-2 mr-2 mt-4 p-4 border rounded-lg border-y  text-white text-lg text-shadow-lg font-semibold items-center">
+      {/* Ready to Ship & Picked side by side */}
+      <div className="flex flex-row justify-evenly ml-2 mr-2 mt-4 p-4 border-x rounded-lg  text-white text-lg text-shadow-lg font-semibold items-center">
+        <div className="flex flex-row gap-4">
           <div className="flex flex-col bg-gray-800/30 shadow-md shadow-white p-4 rounded-lg">
-            <h1 className="text-2xl">INVENTORY HEALTH:</h1>
+            <h1 className="text-2xl">READY TO SHIP:</h1>
+            <p className="ml-2 text-xl"> Orders: {openOrderCount} </p>
+            <p className="ml-2 text-xl">Total Revenue: ${orderTotal} </p>
+            <p>Number of Orders by Type/Revenue: </p>
           </div>
-          <div className="flex flex-col bg-gray-800/40 shadow-md shadow-white p-4 rounded-lg">
-            <h1 className="text-2xl">NEEDS REPLENISHMENT:</h1>
+          <div className="flex flex-col bg-gray-800/30 shadow-md shadow-white p-4 rounded-lg">
+            <h1 className="text-2xl">PICKED:</h1>
+            <p className="ml-2 text-xl"> Orders: {/*{pickedOrderCount}*/} </p>
+            <p className="ml-2 text-xl">
+              Total Revenue: {/*${pickedOrderTotal}*/}
+            </p>
+            <p>Number of Orders by Type/Revenue: </p>
           </div>
+        </div>
+      </div>
+      {/* Shipped below, full width */}
+      <div className="flex flex-row justify-evenly ml-2 mr-2 mt-4 p-4 border-x rounded-lg  text-white text-lg text-shadow-lg font-semibold items-center">
+        <div className="flex flex-col bg-gray-800/40 shadow-md shadow-white p-4 rounded-lg">
+          <h1 className="text-2xl">SHIPPED:</h1>
+          <p className="ml-2 text-xl"> Orders: {/*{shippedOrderCount}*/} </p>
+          <p className="ml-2 text-xl">
+            Total Revenue: {/*${shippedOrderTotal}*/}
+          </p>
+          <p>Shipping Revenue vs Shipping Cost</p>
+        </div>
+      </div>
+      {/* Inventory Health & Needs Replenishment side by side */}
+      <div className="flex flex-row justify-evenly ml-2 mr-2 mt-4 p-4 border-x rounded-lg text-white text-lg text-shadow-lg font-semibold items-center">
+        <div className="flex flex-col bg-gray-800/30 shadow-md shadow-white p-4 rounded-lg">
+          <h1 className="text-2xl">INVENTORY HEALTH:</h1>
+        </div>
+        <div className="flex flex-col bg-gray-800/40 shadow-md shadow-white p-4 rounded-lg">
+          <h1 className="text-2xl">NEEDS REPLENISHMENT:</h1>
+        </div>
+      </div>
+      <div className="m-4">
+        <h1 className="text-xl">AVERAGE OUTPUT:</h1>
+      </div>
+      <div className="flex flex-row justify-evenly ml-2 mr-2 mt-4 p-4 rounded-lg border-x  text-white text-lg text-shadow-lg font-semibold items-center">
+        <div className="flex flex-col bg-gray-800/40 shadow-md shadow-white p-4 rounded-lg">
+          <h1 className="text-2xl">DAILY:</h1>
+          <p className="ml-2 text-xl"> Picked: {/*{dailyPicked}*/} </p>
+          <p className="ml-2 text-xl">Shipped: {/*${dailyShipped}*/}</p>
+        </div>
+        <div className="flex flex-col bg-gray-800/40 shadow-md shadow-white p-4 rounded-lg">
+          <h1 className="text-2xl">WEEKLY:</h1>
+          <p className="ml-2 text-xl"> Picked: {/*{weeklyPicked}*/} </p>
+          <p className="ml-2 text-xl">Shipped: {/*${weeklyShipped}*/}</p>
+        </div>
+        <div className="flex flex-col bg-gray-800/40 shadow-md shadow-white p-4 rounded-lg">
+          <h1 className="text-2xl">MONTHLY:</h1>
+          <p className="ml-2 text-xl"> Picked: {/*{monthlyPicked}*/} </p>
+          <p className="ml-2 text-xl">Shipped: {/*${monthlyShipped}*/}</p>
+        </div>
+        <div className="flex flex-col bg-gray-800/40 shadow-md shadow-white p-4 rounded-lg">
+          <h1 className="text-2xl">YEAR TO DATE:</h1>
+          <p className="ml-2 text-xl"> Picked: {/*{yearlyPicked}*/} </p>
+          <p className="ml-2 text-xl">Shipped: {/*${yearlyShipped}*/}</p>
         </div>
       </div>
     </div>
