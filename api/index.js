71a8e9801bb961961aadf7d4ba313a6053335c44
finnensley <finniganensley@gmail.com>
2026-02-13 14:32:36 -0700

    modified api/index to conditionally use app.listen locally

--- api/index.js
+++ api/index.js
@@ -1248,10 +1248,12 @@ app.get(/.*/, (req, res) => {
   res.sendFile(join(__dirname, "..", "dist", "index.html"));
 });
 
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
+// Start server locally (Vercel ignores this and uses export instead)
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
 
 // Export for Vercel
 export default app;
