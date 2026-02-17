a57c4bcaf65001b5fb267e6de77b4acecd4c7f64
finnensley <finniganensley@gmail.com>
2026-02-16 21:26:46 -0700

    changed sign up form to be a modal and disabled for now

--- src/pages/auth/auth.jsx
+++ src/pages/auth/auth.jsx
@@ -26,6 +26,7 @@ const AuthPage = () => {
   const [signUpUserName, setSignUpUserName] = useState("");
   const [signUpEmail, setSignUpEmail] = useState("");
   const [signUpPassword, setSignUpPassword] = useState("");
+  const [showSignUp, setShowSignUp] = useState(false);
 
   // fetch
   const handleLogIn = async (e) => {
@@ -113,42 +114,62 @@ const AuthPage = () => {
           Enter
         </button>
       </form>
+      <button
+        type="button"
+        onClick={() => setShowSignUp(!showSignUp)}
+        className="m-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+      >
+        {showSignUp ? "Back to Login" : "Sign Up"}
+      </button>
       {error && <div className="text-red-500">{error}</div>}
-      <div className="flex flex-col">
-        <h1 className="text-2xl m-2">Create A New Account</h1>
-        <form
-          className="flex flex-col m-2 p-2 items-center"
-          onSubmit={handleSignUp}
-        >
-          <label>User Name</label>
-          <input
-            className="ml-2 w-fit pl-2 border rounded-lg"
-            value={signUpUserName}
-            onChange={(e) => setSignUpUserName(e.target.value)}
-            type="text"
-            required
-          ></input>
-          <label>Email</label>
-          <input
-            className="ml-2 w-fit pl-2 border rounded-lg"
-            value={signUpEmail}
-            onChange={(e) => setSignUpEmail(e.target.value)}
-            type="email"
-            required
-          ></input>
-          <label>Password</label>
-          <input
-            className="ml-2 w-fit pl-2  border rounded-lg"
-            value={signUpPassword}
-            onChange={(e) => setSignUpPassword(e.target.value)}
-            type="password"
-            required
-          ></input>
-          <button type="Submit" className="m-4">
-            Click Here To Set Up A New Account
-          </button>
-        </form>
-      </div>
+      <AnimatePresence>
+        {showSignUp && (
+          <motion.div
+            className="flex flex-col"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.3 }}
+          >
+            <h1 className="text-2xl m-2">Create A New Account</h1>
+            <form
+              className="flex flex-col m-2 p-2 items-center"
+              onSubmit={handleSignUp}
+            >
+              <label>User Name</label>
+              <input
+                className="ml-2 w-fit pl-2 border rounded-lg"
+                value={signUpUserName}
+                onChange={(e) => setSignUpUserName(e.target.value)}
+                type="text"
+                disabled
+                required
+              ></input>
+              <label>Email</label>
+              <input
+                className="ml-2 w-fit pl-2 border rounded-lg"
+                value={signUpEmail}
+                onChange={(e) => setSignUpEmail(e.target.value)}
+                type="email"
+                disabled
+                required
+              ></input>
+              <label>Password</label>
+              <input
+                className="ml-2 w-fit pl-2  border rounded-lg"
+                value={signUpPassword}
+                onChange={(e) => setSignUpPassword(e.target.value)}
+                type="password"
+                disabled
+                required
+              ></input>
+              <button type="Submit" className="m-4" disabled>
+                Click Here To Set Up A New Account
+              </button>
+            </form>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </motion.div>
   );
 };
